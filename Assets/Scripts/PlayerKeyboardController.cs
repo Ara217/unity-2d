22a650e91979ed52632df5b861391f837ed19de5
araQZ <ara@qustomerz.com>
2019-05-16 19:49:21 +0400

    Added scripts and new animations

---
+++ Assets/Scripts/PlayerKeyboardController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyboardController : MonoBehaviour
+{
+
+    public Main Main;
+
+    private void Start()
+    {
+
+        Main = Main == null ? GetComponent<Main>() : Main;
+        if (Main == null)
+        {
+            Debug.LogError("Player not set to controller");
+        }
+    }
+
+    private void Update()
+    {
+        if (Main != null)
+        {
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                Main.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Main.MoveLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Main.Jump();
+            }
+        }
+    }
+}
